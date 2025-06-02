263394355ab68c055a15d618715d03fbee90ffc2
Rescobar226 <160563775+Rescobar226@users.noreply.github.com>
2025-06-02 00:22:27 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,52 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
-`default_nettype none
-
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module tt_um_Rescobar226 (
+    input  wire        clk,
+    input  wire        rst_n,
+    input  wire        ena,
+    input  wire [7:0]  ui_in,
+    output wire [7:0]  uo_out,
+    input  wire [7:0]  uio_in,
+    output wire [7:0]  uio_out,
+    output wire [7:0]  uio_oe
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    wire Sen = ui_in[0];
+    wire SE  = ui_in[1];
+    wire LA  = ui_in[2];
+    wire LC  = ui_in[3];
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    reg [3:0] S = 4'b0000;
+    reg [3:0] S_n;
+
+    always @(*) begin
+        S_n[3] = ~S[3] & S[2] & ~S[1] & ~S[0] & ~Sen & ~SE & LA;
+        S_n[2] = ~S[3] & ~S[2] & S[1] & ~S[0] & Sen & ~SE & ~LC;
+        S_n[1] = (S[3] & ~S[2] & ~S[1] & ~S[0] & ~Sen & SE & ~LA & ~LC) |
+                 (~S[3] & ~S[2] & ~S[1] & S[0] & Sen & ~SE & ~LA);
+        S_n[0] = (S[3] & ~S[2] & ~S[1] & ~S[0] & ~Sen & ~SE & ~LA & LC) |
+                 (~S[3] & ~S[2] & ~S[1] & ~S[0] & Sen & ~SE & ~LA & LC);
+    end
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            S <= 4'b0000;
+        else if (ena)
+            S <= S_n;
+    end
+
+    wire MA = (S == 4'b0010);
+    wire MC = (S == 4'b0100);
+
+    assign uo_out[0] = MA;
+    assign uo_out[1] = MC;
+    assign uo_out[2] = S[0];
+    assign uo_out[3] = S[1];
+    assign uo_out[4] = S[2];
+    assign uo_out[5] = S[3];
+    assign uo_out[6] = 1'b0;
+    assign uo_out[7] = 1'b0;
+
+    // Si no usas los puertos bidireccionales, desactívalos así:
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
 
 endmodule
